02931530e5d45d1adc7c874a5ab472a3974200df
Alex Marinkevych <24780101+alexey-marinkevich@users.noreply.github.com>
2022-12-10 20:31:59 +0100

    Add .env variables

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(private config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgres://postgres:postgrespw@localhost:49153/mydb?schema=public',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
