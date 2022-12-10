cc831721fe8108515753b1c5fb88341d652b5698
Alex Marinkevych <24780101+alexey-marinkevich@users.noreply.github.com>
2022-12-10 22:05:51 +0100

    Add token to signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,20 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { Res } from '@nestjs/common/decorators';
 import { AuthDto } from 'src/dto';
 import { AuthService } from './auth.service';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup(@Body() body: AuthDto) {
-    return this.authService.signup(body);
+  async signup(
+    @Body() body: AuthDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const token = await this.authService.signup(body);
+    return res.set('x-auth-header', token).status(200).end();
   }
 
   @Post('signin')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,16 @@ import { Injectable, ForbiddenException } from '@nestjs/common';
 import { AuthDto } from 'src/dto';
 import * as argon from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt/dist';
+import { ConfigService } from '@nestjs/config/dist';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signup(body: AuthDto) {
     const { email, password } = body;
@@ -22,10 +28,17 @@ export class AuthService {
 
     const hashedPassword = await argon.hash(password);
 
-    return this.prisma.user.create({
+    const createdUser = await this.prisma.user.create({
       data: { email, password: hashedPassword },
-      select: { email: true, id: true },
+      select: { id: true, email: true },
     });
+    console.log(createdUser);
+    const token = await this.signToken(
+      createdUser.id.toString(),
+      createdUser.email,
+    );
+
+    return `Bearer ${token}`;
   }
 
   async signin(body: AuthDto) {
@@ -49,5 +62,17 @@ export class AuthService {
 
     delete user.password;
     return user;
+  }
+
+  signToken(userId: string, email: string) {
+    const data = {
+      sub: userId,
+      email,
+    };
+
+    return this.jwt.signAsync(data, {
+      expiresIn: '15m',
+      secret: this.config.get('JWT_SECRET_KEY'),
+    });
   }
 }
