7b22016c3d81eddba1ae0e1803674729968dff5c
Alex Marinkevych <24780101+alexey-marinkevich@users.noreply.github.com>
2022-12-11 16:01:49 +0100

    Add about me route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get, Post, Param } from '@nestjs/common';
+import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guard';
 import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  @Get('/about-me')
+  aboutMe() {
+    return this.userService.aboutMe();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('/:id')
   getUserById(@Param('id') id: string) {
     return this.userService.getUserById(id);
   }
