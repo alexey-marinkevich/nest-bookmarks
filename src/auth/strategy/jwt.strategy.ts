4cf3976253cc1b83f153f2b8b65c728731c117c8
Alex Marinkevych <24780101+alexey-marinkevich@users.noreply.github.com>
2022-12-11 15:23:07 +0100

    Add validate to a strategy

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -4,11 +4,15 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(private config: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('JWT_SECRET_KEY'),
     });
+  }
+
+  async validate(payload) {
+    return payload;
   }
 }
