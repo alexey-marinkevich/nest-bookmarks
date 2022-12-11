65eea88d71e2b9a2857dd3bf88c754eff3e72193
Alex Marinkevych <24780101+alexey-marinkevich@users.noreply.github.com>
2022-12-11 14:54:24 +0100

    Update auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,28 +1,26 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { Res } from '@nestjs/common/decorators';
 import { AuthDto } from 'src/dto';
 import { AuthService } from './auth.service';
-import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  async signup(
-    @Body() body: AuthDto,
-    @Res({ passthrough: true }) res: Response,
-  ) {
+  async signup(@Body() body: AuthDto) {
     const token = await this.authService.signup(body);
-    return res.set('x-auth-token', token).status(200).end();
+
+    return {
+      'x-auth-token': token,
+    };
   }
 
   @Post('signin')
-  async signin(
-    @Body() body: AuthDto,
-    @Res({ passthrough: true }) res: Response,
-  ) {
+  async signin(@Body() body: AuthDto) {
     const token = await this.authService.signin(body);
-    return res.set('x-auth-token', token).status(200).end();
+
+    return {
+      'x-auth-token': token,
+    };
   }
 }
